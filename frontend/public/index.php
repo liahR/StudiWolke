49594d34a30e730f39eefe0f55adf31f98409d51
Janni <128581278+jannaschweigmann@users.noreply.github.com>
2023-04-12 08:40:43 +0200

    Update index.php

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -67,52 +67,52 @@ if ($statement->execute()) {
     });
 
     // Button zum Sortieren nach Namen
-    echo '<button onclick="sortByName()">Nach Namen sortieren</button>';
+    echo <button onclick="sortByName()">Nach Namen sortieren</button>;
 
     // Suchfeld
-    echo '<input type="text" id="search-input" oninput="searchFolders()" placeholder="Suche nach Dateien...">';
+    echo <input type="text" id="search-input" oninput="searchFolders()" placeholder="Suche nach Dateien...">;
 
     // Liste der Ordner
     echo '<ul id="ordner-liste">';
     foreach ($rows as $row) {
-        echo '<li>';
-        echo '<img src="cloud-ordner.png" alt="Ordner-Icon">';
-        echo '<h2><a href="in_Ordner.php=' . $row['OrdnerId'] . '">' . $row['Ordnername_original'] . '</a></h2>';
-        echo '<a href="delete_ordner_do.php=' . $row['OrdnerId'] . '">Löschen</a><br>';
-        echo '</li>';
+        echo <li>;
+        echo <img src="cloud-ordner.png" alt="Ordner-Icon">;
+        echo <h2><a href="in_Ordner.php=' . $row['OrdnerId'] . '">' . $row['Ordnername_original'] . '</a></h2>;
+        echo <a href="delete_ordner_do.php=' . $row['OrdnerId'] . '">Löschen</a><br>;
+        echo </li>;
     }
-    echo '</ul>';
+    echo </ul>;
 
     // JavaScript-Code zum Sortieren und Suchen der Liste
-    echo '<script>';
-    echo 'function sortByName() {';
-    echo '  var list = document.getElementById("ordner-liste'; 
-    echo '  var items = list.getElementsByTagName("li");';
-    echo '  var arr = Array.prototype.slice.call(items);';
-    echo '  arr.sort(function(a, b) {';
-    echo '    var aName = a.getElementsByTagName("h2")[0].textContent;';
-    echo '    var bName = b.getElementsByTagName("h2")[0].textContent;';
-    echo '    return aName.localeCompare(bName);';
-    echo '  });';
-    echo '  for (var i = 0; i < arr.length; i++) {';
-    echo '    list.appendChild(arr[i]);';
-    echo '  }';
-    echo '}';
-    echo 'function searchFolders() {';
-    echo '  var input = document.getElementById("search-input");';
-    echo '  var filter = input.value.toUpperCase();';
-    echo '  var list = document.getElementById("ordner-liste");';
-    echo '  var items = list.getElementsByTagName("li");';
-    echo '  for (var i = 0; i < items.length; i++) {';
-    echo '    var name = items[i].getElementsByTagName("h2")[0].textContent;';
-    echo '    if (name.toUpperCase().indexOf(filter) > -1) {';
-    echo '      items[i].style.display = "";';
-    echo '    } else {';
-    echo '      items[i].style.display = "none";';
-    echo '    }';
-    echo '  }';
-    echo '}';
-    echo '</script>';
+    echo <script>;
+    echo function sortByName() {;
+    echo   var list = document.getElementById("ordner-liste");
+    echo   var items = list.getElementsByTagName("li");
+    echo   var arr = Array.prototype.slice.call(items);
+    echo   arr.sort(function(a, b) {;
+    echo     var aName = a.getElementsByTagName("h2")[0].textContent;
+    echo     var bName = b.getElementsByTagName("h2")[0].textContent;
+    echo     return aName.localeCompare(bName);
+    echo   });
+    echo   for (var i = 0; i < arr.length; i++) {;
+    echo     list.appendChild(arr[i]);
+    echo   };
+    echo };
+    echo function searchFolders() {;
+    echo   var input = document.getElementById("search-input");
+    echo   var filter = input.value.toUpperCase();
+    echo   var list = document.getElementById("ordner-liste");
+    echo   var items = list.getElementsByTagName("li");
+    echo   for (var i = 0; i < items.length; i++) {;
+    echo     var name = items[i].getElementsByTagName("h2")[0].textContent;
+    echo     if (name.toUpperCase().indexOf(filter) > -1) {;
+    echo       items[i].style.display = "";;
+    echo     } else {;
+    echo       items[i].style.display = "none";
+    echo     };
+    echo   };
+    echo };
+    echo </script>;
 }
     
     //Sollte es nicht geklappt haben, wird eine Fehler Info ausgegeben.

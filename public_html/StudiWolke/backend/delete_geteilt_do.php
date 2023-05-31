cd6c8a8c82c57f0e2f7e12e9503922a34ff003ca
liahR <116581155+liahR@users.noreply.github.com>
2023-05-31 09:26:27 +0200

    löschen teilen

--- public_html/StudiWolke/backend/delete_geteilt_do.php
+++ public_html/StudiWolke/backend/delete_geteilt_do.php
@@ -8,14 +8,14 @@
 
 $pdo  =new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-lr090', 'lr090','eetho6Choh', array('charset'=>'utf8'));
 
-$datei_id = $_POST["datei_id"];
+$teilen_id = $_POST["teilen_id"];
 
 //Dateien aus Datenbank löschen
-$statement = $pdo->prepare("DELETE FROM dateien WHERE datei_id = :datei_id");
-$statement->bindParam(':datei_id', $datei_id);
+$statement = $pdo->prepare("DELETE FROM teilen WHERE teilen_id = :teilen_id");
+$statement->bindParam(':teilen_id', $teilen_id);
 
 if($statement->execute()){
-    header('Location: ../frontend/public/in_ordner.php');
+    header('Location: ../frontend/public/in_geteilt.php');
 }else{
     echo "Datenbank-Fehler";
     echo $statement->errorInfo()[2];

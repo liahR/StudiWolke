39fefa529ebfd665ef67060b6f7dd9a91584180e
liahR <116581155+liahR@users.noreply.github.com>
2023-05-31 13:50:38 +0200

    klen

--- public_html/StudiWolke/frontend/public/index.php
+++ public_html/StudiWolke/frontend/public/index.php
@@ -104,7 +104,7 @@ else {
     <div class="geteilte_ordner"><br>        
     <img src="geteilte-ordner.png" alt=" Geteilte Dateien Ordner-Icon">
     <form action="in_geteilt.php" method="post">
-    <h2><button class="Ordnernamen" type="submit">Geteilte Dateien</button></h2>
+    <h2><button class="ordnernamen" type="submit">Geteilte Dateien</button></h2>
     </form>
     </div>
 
@@ -124,7 +124,7 @@ if ($statement->execute()) {
         //Button für in ordner
         echo '<form action="in_ordner.php" method="post">';
         echo '<input type="hidden" name="ordner_id" value="' . $row['ordner_id'] . '">';
-        echo '<h2><button class="Ordnernamen" type="submit">' . $row['ordnername_original'] . '</button></h2>';
+        echo '<h2><button class="ordnernamen" type="submit">' . $row['ordnername_original'] . '</button></h2>';
         echo '</form>';
         // Lösch-Buttons
         echo "<form action='../../backend/delete_ordner_do.php' method='post'>";

b324081e73efae5cf15672392064429a8ea186bc
Janni <128581278+jannaschweigmann@users.noreply.github.com>
2023-06-08 12:03:07 +0200

    Update hilfe.php

--- public_html/StudiWolke/frontend/public/hilfe.php
+++ public_html/StudiWolke/frontend/public/hilfe.php
@@ -1,106 +1,119 @@
+<?php
+    session_start();
+    // Prüfen ob Benutzer nicht eingeloggt ist + ordner id setzen 
+    if (!isset($_SESSION["benutzer_id"]))
+{
+    header("Location: login.html");
+}
+else {
+    $benutzer_id = $_SESSION["benutzer_id"];
+}
+
+    $pdo = new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de;dbname=u-lr090', 'lr090', 'eetho6Choh', array('charset' => 'utf8'));
+
+?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" id="favicon">
-    <link rel="stylesheet" type="text/css" href="allgemein.css">    
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" id="favicon">
+    <link rel="stylesheet" type="text/css" href="allgemein.css">	
     <title>Support</title>
-    <script>
-        function toggleAnswer(event) {
-            var answer = event.target.nextElementSibling;
-            if (answer.style.display === "none") {
-                answer.style.display = "block";
-            } else {
-                answer.style.display = "none";
-            }
-        }
-    </script>
+	<script>
+		function toggleAnswer(event) {
+			var answer = event.target.nextElementSibling;
+			if (answer.style.display === "none") {
+				answer.style.display = "block";
+			} else {
+				answer.style.display = "none";
+			}
+		}
+	</script>
 </head>
 <body>
 <header>
-    <div class="logo">
-        <a href="index.php"><img src="Logo StudiWolke.png"></a>
-    </div>
-    <div class="header-navigation">
-        <nav>
-            <ul>
-                <br><br><br><br><br><br><br><br>
-                <li class="profilbild">
-                    <?php
-                    // SQL-Abfrage zum Abrufen des Profilbilds des Benutzers
-                    $stmt = $pdo->prepare("SELECT profilbild FROM benutzer WHERE benutzer_id=:benutzer_id");
-                    $stmt->bindValue(':benutzer_id', $_SESSION['benutzer_id']);
-                    if ($stmt->execute()) {
-                        while ($row = $stmt->fetch()) {
-                            if (!empty($row["profilbild"])) {
-                                echo '<a href="account.php"><img src="https://mars.iuk.hdm-stuttgart.de/~lr090/StudiWolke/frontend/profilbilder/' . $row["profilbild"] . '"></a>';
-                            }
-                        }
-                    }
-                    ?>
-                </li>
-                <li>
-                    <div class="support_icon">
-                        <a href="hilfe.php">
-                            <img src="support_icon.png" alt="Support Icon">
-                            <span style="display: none;">Support</span>
-                        </a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-    </div>
+  <div class="logo">
+    <a href="index.php"><img src="Logo StudiWolke.png"></a>
+  </div>
+  <div class="header-navigation"><
+  <nav>
+    <ul><br><br><br><br><br><br><br><br>
+      <li class="profilbild">
+        <?php
+        // SQL-Abfrage zum Abrufen des Profilbilds des Benutzers
+        $stmt = $pdo->prepare("SELECT profilbild FROM benutzer WHERE benutzer_id=:benutzer_id");
+        $stmt->bindValue(':benutzer_id', $_SESSION['benutzer_id']);
+        if ($stmt->execute()) {
+          while ($row = $stmt->fetch()) {
+            if (!empty($row["profilbild"])) {
+              echo '<a href="account.php"><img src="https://mars.iuk.hdm-stuttgart.de/~lr090/StudiWolke/frontend/profilbilder/' . $row["profilbild"] . '"></a>';
+            }
+          }
+        }
+        ?>
+      </li>
+      <li>
+        <div class="support_icon">
+          <a href="hilfe.php">
+            <img src="support_icon.png" alt="Support Icon">
+            <span style="display: none;">Support</span>
+          </a>
+        </div>
+      </li>
+      
+    </ul>
+  </nav>
+  </div>
 </header>
 <main>
-    <h1>Support</h1>
-    <!-- Häufig gestellte Fragen -->
+
+	<h1>Support</h1>
+	<!-- Häufig gestellte Fragen -->
     <h2>Häufig gestellte Fragen:</h2>
-    <div class="faq">
-        <h3 onclick="toggleAnswer(event)">
+	<div class="faq">
+  <h3 onclick="toggleAnswer(event)">
             Wie kann ich mich in das Cloudsystem einloggen?
             <span class="toggle_icon"></span>
-        </h3>
+        </h3>		
         <p>Um sich in das Cloudsystem einzuloggen, gehen Sie auf die Startseite und klicken Sie auf den "Anmelden"-Button. Geben Sie dann Ihre Anmeldedaten ein und klicken Sie auf "Einloggen".</p>
-    </div>
-    
-    <div class="faq">
-        <h3 onclick="toggleAnswer(event)">
-            Wie kann ich Dateien hochladen?
-            <span class="toggle_icon"></span>
-        </h3>
-        <p>Um Dateien hochzuladen, klicken Sie auf den "Dateien hochladen"-Button auf der Startseite. Wählen Sie dann die Datei aus, die Sie hochladen möchten, und klicken Sie auf "Hochladen".</p>
-    </div>
-    
-    <div class="faq">
-        <h3 onclick="toggleAnswer(event)">
-            Wie kann ich meine Dateien verwalten?
-            <span class="toggle_icon"></span>
-        </h3>
-        <p>Um Ihre Dateien zu verwalten, gehen Sie auf die Startseite und klicken Sie auf den jeweiligen Ordner. Hier können Sie Ihre Dateien anzeigen oder löschen.</p>
-    </div>
-    <!-- Formular zum Stellen von Fragen -->
-    <form action="../../backend/support_do.php" method="post">
-        <label for="frage">Stellen Sie Ihre Frage:</label><br>
-        <textarea name="frage" id="frage" rows="8" cols="80" required></textarea><br>
-        <button type="submit">Frage stellen</button>
-    </form>
-</main>    
+	</div>
+	
+	<div class="faq">
+		<h3 onclick="toggleAnswer(event)">
+          Wie kann ich Dateien hochladen?
+          <span class="toggle_icon"></span>
+    </h3>
+		<p>Um Dateien hochzuladen, klicken Sie auf den "Dateien hochladen"-Button auf der Startseite. Wählen Sie dann die Datei aus, die Sie hochladen möchten, und klicken Sie auf "Hochladen".</p>
+	</div>
+	
+	<div class="faq">
+		<h3 onclick="toggleAnswer(event)">
+      Wie kann ich meine Dateien verwalten?
+      <span class="toggle_icon"></span>
+    </h3>
+		<p>Um Ihre Dateien zu verwalten, gehen Sie auf die Startseite und klicken Sie auf den jeweiligen Ordner. Hier können Sie Ihre Dateien anzeigen oder löschen.</p>
+	</div>
+	<!-- Formular zum Stellen von Fragen -->
+		<form action="../../backend/support_do.php" method="post">
+    	<label for="frage">Stellen Sie Ihre Frage:</label><br>
+    	<textarea name="frage" id="frage" rows="8" cols="80" required></textarea><br>
+    	<button type="submit">Frage stellen</button>
+		</form>
+</main>	
 <footer>
     <div class="logo_footer">
-        <a href="index.php"><img src="Logo StudiWolke.png"></a>
-    </div>
+			<a href="index.php"><img src="Logo StudiWolke.png"></a>
+		</div>
     <nav>
-        <ul class="footer_links">
-            <li><a href="impressum.php">IMPRESSUM</a></li>
-            <li><a href="datenschutz.php">DATENSCHUTZ</a></li>
-            <li><a href="agbs.php">AGBs</a></li>
+        <ul class= "footer_links" >
+        <li><a href= "impressum.php">IMPRESSUM</a></li>
+            <li><a href= "datenschutz.php">DATENSCHUTZ</a></li>
+            <li><a href= "agbs.php">AGBs</a></li>  
         </ul>
     </nav>
     <div class="logout_icon">
         <a href="../../backend/logout.php"><img src="logout_icon.png" alt="Logout" /></a>
-    </div>
-    <div class="footer_copyright">
-        <br><br><br><br> &copy; 2023 StudiWolke GmbH & Co. KG
-    </div>
+        </div>	
+    <div class="footer_copyright"><br><br><br><br> &copy; 2023 StudiWolke GmbH & Co. KG</div>
 </footer>
 </body>
 </html>
